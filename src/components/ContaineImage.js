1e68258d59ba9d91f1198faf1321f44fbae24be2
istdavidev <davidlez2mgk@gmail.com>
2022-02-15 23:08:47 -0600

    ✨ add container and styles of header

--- src/components/ContaineImage.js
+++ src/components/ContaineImage.js
@@ -1,29 +1,33 @@
 import cone from '../assets/desktop/image-gallery-cone.jpg'
-
-class ContaineImage extends HTMLElement{
-    constructor(){
+const imgs = {
+    cone
+}
+class ContaineImage extends HTMLElement {
+    constructor() {
         super();
-        this.attachShadow({mode:'open'});
-}
+        this.attachShadow({ mode: 'open' });
+    }
 
-static get styles(){
-    return /*css*/`
-     
+    static get styles() {
+        return /*css*/`
+       this.name{
+           color:red
+       }
     `;
-}
+    }
 
-connectedCallback(){
-    this.name = this.getAttribute('name')
-     this.render();
-}
+    connectedCallback() {
+        this.namecss = this.getAttribute('name')
+        this.render();
+    }
 
-render(){
-    this.shadowRoot.innerHTML =/*html*/`
+    render() {
+        this.shadowRoot.innerHTML =/*html*/`
         <style>${ContaineImage.styles}</style>
- <div class='${this.name}'>
-     hola munod 
+ <div class='${this.namecss}'>
+     ${imgs[this.namecss]}
 </div>
     `;
+    }
 }
-}
-customElements.define('containe-image',ContaineImage);
+customElements.define('containe-image', ContaineImage);
